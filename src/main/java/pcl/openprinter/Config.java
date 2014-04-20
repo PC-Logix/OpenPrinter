410cf7be8aebc1b7e5eb4ae42c83fc2f9d57d190
Caitlyn <Michiyo.Ravencroft@gmail.com>
2014-04-20 18:34:26 -0500

    4000 uses default.

--- src/main/java/pcl/openprinter/Config.java
+++ src/main/java/pcl/openprinter/Config.java
@@ -14,7 +14,7 @@ public class Config
 { 
     public final boolean render3D;
 	
-	private int defaultInkUse = 100;
+	private int defaultInkUse = 4000;
 	public final int printerInkUse;
 
 	private boolean defaultEnableMUD = true;
@@ -23,6 +23,9 @@ public class Config
     public Config(Configuration config)
     {
         config.load();
+        if (config.get("options", "inkUses", defaultInkUse).getInt(defaultInkUse) == 100) {
+        	config.get("options", "inkUses", defaultInkUse, "How many times you can print with a ink cartridge").set(defaultInkUse);
+        }
         printerInkUse = config.get("options", "inkUses", defaultInkUse, "How many times you can print with a ink cartridge").getInt(defaultInkUse);
         render3D = config.get("options", "Render3D", true, "Should we use the 3D Model, or a block").getBoolean(true);
 		enableMUD = config.get("options", "enableMUD", true, "Enable the Update Checker? Disabling this will remove all traces of the MUD.").getBoolean(true);
