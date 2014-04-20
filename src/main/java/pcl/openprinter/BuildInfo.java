69becd6336cf3ec6f014bfc4c1cbd8d612b5f604
Caitlyn <Michiyo.Ravencroft@gmail.com>
2014-04-20 18:36:49 -0500

    Ugh.

--- src/main/java/pcl/openprinter/BuildInfo.java
+++ src/main/java/pcl/openprinter/BuildInfo.java
@@ -7,28 +7,11 @@ package pcl.openprinter;
  * @author AfterLifeLochie, stolen from LanteaCraft, another fine PC-Logix Minecraft mod.
  */
 public class BuildInfo {
-	public static final String modName = "LanteaCraft";
-	public static final String modID = "LanteaCraft";
+	public static final String modName = "OpenPrinter";
+	public static final String modID = "openprinter";
 
 	public static final String versionNumber = "@VERSION@";
 	public static final String buildNumber = "@BUILD@";
-
-	/**
-	 * Enable or disable the SoundSystem debugging; often this is useful for
-	 * recording all the operations on the SoundDevice, meaning that clients
-	 * aren't slammed with logging if they don't need to be.
-	 */
-	public static final boolean SS_DEBUGGING = false && isDevelopmentEnvironment();
-
-	/**
-	 * Enable or disable network traffic dumping mode.
-	 */
-	public static final boolean NET_DEBUGGING = false && isDevelopmentEnvironment();
-	
-	/**
-	 * Enable or disable asset and configuration access dumping.
-	 */
-	public static final boolean ASSET_DEBUGGING = false && isDevelopmentEnvironment();
 
 	public static int getBuildNumber() {
 		if (buildNumber.equals("@" + "BUILD" + "@"))
