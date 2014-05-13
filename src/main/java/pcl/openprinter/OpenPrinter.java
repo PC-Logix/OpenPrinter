fcbe1b5c13d512797f5908fa26200862d3dd22d8
Caitlyn <Michiyo.Ravencroft@gmail.com>
2014-05-13 17:50:13 -0500

    Fixed OreDict recipes.

--- src/main/java/pcl/openprinter/OpenPrinter.java
+++ src/main/java/pcl/openprinter/OpenPrinter.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package pcl.openprinter;
 
 /**
@@ -10,8 +7,16 @@ package pcl.openprinter;
 import java.net.URL;
 import java.util.logging.Logger;
 
+import li.cil.oc.api.Items;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.Configuration;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import pcl.openprinter.blocks.Printer;
-import pcl.openprinter.tileentity.PrinterTE;
 import pcl.openprinter.gui.PrinterGUIHandler;
 import pcl.openprinter.items.ItemPrinterBlock;
 import pcl.openprinter.items.PrintedPage;
@@ -19,17 +24,7 @@ import pcl.openprinter.items.PrinterInkBlack;
 import pcl.openprinter.items.PrinterInkColor;
 import pcl.openprinter.items.PrinterPaper;
 import pcl.openprinter.items.PrinterPaperRoll;
-import pcl.openprinter.BuildInfo;
-import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.Property;
-import net.minecraftforge.oredict.OreDictionary;
-import net.minecraftforge.oredict.ShapedOreRecipe;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemStack;
+import pcl.openprinter.tileentity.PrinterTE;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -37,15 +32,10 @@ import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.ModContainer;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
-import li.cil.oc.api.Blocks;
-import li.cil.oc.api.CreativeTab;
-import li.cil.oc.api.Items;
 
 @Mod(modid=OpenPrinter.MODID, name="OpenPrinter", version=BuildInfo.versionNumber + "." + BuildInfo.buildNumber, dependencies = "after:OpenComputers")
 @NetworkMod(clientSideRequired=true)
@@ -134,7 +124,6 @@ public class OpenPrinter {
         @EventHandler
     	public void load(FMLInitializationEvent event)
     	{
-        	ItemStack nuggetIron   = Items.IronNugget;
         	ItemStack redstone     = new ItemStack(Item.redstone);
         	ItemStack microchip    = Items.MicrochipTier1;
         	ItemStack pcb		   = Items.PrintedCircuitBoard;
@@ -143,7 +132,6 @@ public class OpenPrinter {
         	ItemStack greenInk	   = new ItemStack(Item.dyePowder, 1, 2);
         	ItemStack blueInk	   = new ItemStack(Item.dyePowder, 1, 4);
         	ItemStack paper        = new ItemStack(Item.paper);
-        	ItemStack lprinterPaper	= new ItemStack(printerPaper,64);
         	ItemStack stackPaper	= new ItemStack(Item.paper,64);
 
         	
@@ -151,27 +139,22 @@ public class OpenPrinter {
         			"IRI",
         			"MPM",
         			"IRI",
-        			'I', nuggetIron, 'R', redstone, 'M', microchip, 'P', pcb));
+        			'I', "nuggetIron", 'R', redstone, 'M', microchip, 'P', pcb));
         	
         	GameRegistry.addRecipe(new ShapedOreRecipe( new ItemStack(printerInkBlack, 1), 
         			"BBB",
         			" I ",
-        			'B', blackInk, 'I', nuggetIron));
+        			'B', blackInk, 'I', "nuggetIron"));
         	
         	GameRegistry.addRecipe(new ShapedOreRecipe( new ItemStack(printerInkColor, 1), 
         			"RGB",
         			" I ",
-        			'R', redInk, 'G', greenInk, 'B', blueInk, 'I', nuggetIron));
-        	
-        	
-        	GameRegistry.addRecipe( new ItemStack(printerPaperRoll, 1), 
-        			"PP",
-        			"PP",
-        			'P', lprinterPaper);
+        			'R', redInk, 'G', greenInk, 'B', blueInk, 'I', "nuggetIron"));
         	
         	GameRegistry.addRecipe( new ItemStack(printerPaperRoll, 1), 
-        			"PP",
-        			"PP",
+        			" P ",
+        			"P P",
+        			" P ",
         			'P', stackPaper);
         	
         	GameRegistry.addRecipe( new ItemStack(printerInkColor, 1),
