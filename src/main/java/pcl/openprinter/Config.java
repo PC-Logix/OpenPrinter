f2b5a1d01cd7929804250d5e88d07074a3edb014
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-06-02 18:18:00 -0500

    Added enableNameTag to disable printing name tags

--- src/main/java/pcl/openprinter/Config.java
+++ src/main/java/pcl/openprinter/Config.java
@@ -13,16 +13,16 @@ public class Config
 
 	private int defaultInkUse = 4000;
 	public final int printerInkUse;
-
-	private boolean defaultEnableMUD = true;
 	public final boolean enableMUD;
+    public final boolean enableNameTag;
 
     public Config(Configuration config)
     {
         config.load();
         printerInkUse = config.get("options", "inkUses", defaultInkUse, "How many times you can print with a ink cartridge").getInt(defaultInkUse);
         render3D = config.get("options", "Render3D", true, "Should we use the 3D Model, or a block").getBoolean(true);
-		enableMUD = config.get("options", "enableMUD", true, "Enable the Update Checker? Disabling this will remove all traces of the MUD.").getBoolean(true);
+	enableMUD = config.get("options", "enableMUD", true, "Enable the Update Checker? Disabling this will remove all traces of the MUD.").getBoolean(true);
+        enableNameTag = config.get("options", "enableNameTag", true, "Allows the printer to print Name Tags, configurable because this usually costs XP.").getBoolean(true);
         if( config.hasChanged() )
         {
             config.save();
